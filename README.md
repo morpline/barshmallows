de8ef7bab3850d5a4f3ca5ce11e23be4fcc511cb
morpline <118403086+morpline@users.noreply.github.com>
2023-01-09 15:13:56 -0700

    Update Alpha 3
    See patch notes in README.md

--- README.md
+++ README.md
@@ -3,6 +3,20 @@
 
 created by Ririe Nielsen
 inspired by Rhys Nielsen
+# Update Alpha 3 Notes
+Added images to main branch
+Changed how the game does some behind-the-scenes stuff
+Made the game less cpu hungry; made certain thangs happen only when necessary, like:
+    updating barshmallow count
+    updating money count
+    updating barshmallows
+    updating selected barshmallows
+Started building the saving and loading system; 
+    saving works
+    loading money works
+    loading barshmallows doesn't
+Added "New Game" button
+Simplified the Barshmallow construct
 # Update Alpha 2 Notes
 Fixed title from "Barhsmallows" to "Barshmallows"
 Adjusted breeding to make it take longer to get better barshmallows
--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 
 const colors = ["Salmon","Red","Scarlet","Orange","Brown","Yellow","Lime green","Green","Cyan","Blue","Lavender","Violet","Mauve"];
 const colorHex = ["#ff5577","red","#ffaa3a","orange","#88aa00","yellow","#aaff55","green","#00dddd","blue","#ffaaff","violet",""];
-const shapes = ["Cubic","Spherical","Trapezoidal","Hexagonal","Toroidal","Like an 'E'","Amorphous","Cylindrical","Great"];
+const colorRGBA = ["rgba(255,85,119,0.5)","rgba(255,0,0,0.5)","rgba(255,170,58,0.5)","rgba(255,127,0,0.5)","rgba(136,170,0,0.5)","rgba(255,255,0,0.5)","rgba(170,255,85,0.5)","rgba(0,255,0,0.5)","rgba(0,221,221,0.5)","rgba(0,0,255,0.5)","#rgba(255,175,255,0.5)","rgba(255,0,255,0.5)",""];
+const shapes = ["Cubic","Spherical","Trapezoidal","Hexagonal","Toroidal","Obtuse","Amorphous","Cylindrical","Great"];
+const shapeImages = ["images/shapes/cubic marshmallow.png","images/shapes/spherical marshmallow.png","images/shapes/trapezoidal marshmallow.png","images/shapes/hexagonal marshmallow.png","images/shapes/toroidal marshmallow.png","images/shapes/obtuse marshmallow.png","images/shapes/amorphous marshmallow.png","images/shapes/cylindrical marshmallow.png","images/shapes/giant marshmallow.png"];
 const textures = ["Lumpy","Smooth","Rubbery","Rough","Fluffy","Metallic","Cold","Hot","Polygonal"];
+const textureImages = ["images/textures/lumpy marshmallow.png","images/textures/smooth marshmallow.png","images/textures/rubbery marshmallow.png","images/textures/rough marshmallow.png","images/textures/fluffy marshmallow.png","images/textures/metal marshmallow.png","images/textures/transparent marshmallow.png","images/textures/rock marshmallow.png","images/textures/felt marshmallow.png"];
+const normalImageSrc = "images/normal marshmallow.png"
 let barshmallowContainer = document.getElementById("barshmallows");
 let barshmallowId = 0;
 class Barshmallow {
-    constructor (height=Math.random*20, color=0, shape=0, luck=0.5,visible=true,texture=-1) {
+    constructor (height=(Math.random()*20), color=0, shape=0, luck=0.5,visible=true,texture=-1) {
         this.height=height;
         this.color=color;
         this.shape=shape;
@@ -18,25 +22,40 @@ class Barshmallow {
         //console.log(this.id);
         this.div = document.createElement("div");
         this.div.id=(this.id);
-    }
-    update () {
-
-        //selected.push(0);
-
-          
         this.div.className = "barshmallow";
-
+        let barshmallowImageBox = document.createElement("div");
+        let barshmallowImage = document.createElement("img");
+        barshmallowImageBox.classList.add("img");
+        let barshmallowImageSrc = () => {
+            if(this.shape == -1 && this.texture == -1)
+            {
+                return normalImageSrc;
+            } else if (this.shape > this.texture) {
+                return shapeImages[this.shape];
+            } else {
+                return textureImages[this.texture];
+            }
+        }
+        //barshmallowImage.style.backgroundImage=`url("${barshmallowImageSrc()}")`;
+        barshmallowImage.src=barshmallowImageSrc();
+        //console.log(`url("${barshmallowImageSrc()}")`);
+        this.div.style.boxShadow = `0 0px 20px 20px ${colorRGBA[this.color]} inset`;
+        barshmallowImageBox.append(barshmallowImage);
+        this.div.append(barshmallowImageBox);
+    
+    
         let barshmallowName = document.createElement("p");
         barshmallowName.innerHTML = "Barshmallow #"+(this.id+1);
+        this.div.append(barshmallowName);
+    
         let barshmallowShape = document.createElement("p");
-        
         barshmallowShape.innerHTML = "Shape: "+(this.shape==-1?"Normal":shapes[this.shape]);
         this.div.append(barshmallowShape);
-
+    
         let barshmallowColor = document.createElement("p");
         barshmallowColor.innerHTML = "Color: "+colors[this.color];
         
-
+    
         this.div.append(barshmallowColor);
         
         let barshmallowTexture = document.createElement("p");
@@ -46,25 +65,23 @@ class Barshmallow {
             barshmallowTexture.innerHTML = "Texture: "+"Normal";
         }
         this.div.append(barshmallowTexture);
-
+    
         let barshmallowheight = document.createElement("p");
         barshmallowheight.innerHTML = "Height: "+Math.round(this.height);
         this.div.append(barshmallowheight);
-
+    
         let barshmallowWorth = document.createElement("p");
         //barshmallowWorth.innerHTML = "Worth: $"+Math.round((this.height*100+(this.color%2)*500+this.shape*100)*this.luck);
         barshmallowWorth.innerHTML = "Worth: $"+Math.round((this.height*100+(this.color%2)*500+this.shape*100));
         this.div.append(barshmallowWorth);
-
-        let selectButton = document.createElement("input");
-        selectButton.type="button";
-        selectButton.className="barshbutton";
-        selected[this.id]?selectButton.value="Select":selectButton.value="Select";
-        //selectButton.onclick=function() {select(this.id)};
-        selectButton.addEventListener("click",() => {this.toggle(this.id,this.div)},false);
-        this.div.append(selectButton);
-
-        barshmallowContainer.append(this.div);
+    
+        // let selectButton = document.createElement("input");
+        // selectButton.type="button";
+        // selectButton.className="barshbutton";
+        // selected[this.id]?selectButton.value="Select":selectButton.value="Select";
+        // selectButton.onclick=function() {select(this.id)};
+        this.div.addEventListener("click",() => {this.toggle(this.id,this.div)},false);
+        // this.div.append(selectButton);
     }
     toggle(idee,deeeyevee){
         //this.div=document.getElementById(`${idee}`);
@@ -78,11 +95,13 @@ class Barshmallow {
 
     }
 }
-let barshmallows = [new Barshmallow(Math.random()*20,3,-1,0.5000001,true,-1)];
+let money = JSON.parse(localStorage.getItem("money")) || 1000;
+//let barshmallows = [new Barshmallow(Math.random()*20,3,-1,0.5000001,true,-1)];
+let barshmallows = JSON.parse(localStorage.getItem('barshmallows')) || [new Barshmallow(Math.random()*20,3,-1,0.5000001,true,-1)];
 barshmallowId++;
 let numOfBarshmallows = 1;
 let selected= [false];
-barshmallows[0].update();
+// barshmallows[0].createDiv();
 let buyCooldown = 0;
 let buyPrice = 100;
 let breeding = false;
@@ -93,10 +112,38 @@ let mom = -1;
 let selecteds = 0;
 let breedDad = 0;
 let breedMom = 0;
+// //BEGIN TEST SPACE
+// let beans = document.createElement("div");
+// let tubers = document.createElement("p");
+// beans.innerHTML = "beans";
+// tubers.text = "tubers";
+// barshmallowContainer.append(beans);
+// barshmallowContainer.append(tubers);
+// //barshmallowContainer.children[0].remove();
+// barshmallowContainer.insertAdjacentElement("beforebegin",beans)
+// console.log(barshmallowContainer.children);
+// //END TEST SPACE
+
+function populateList(plates = [], platesList) {
+    // platesList = plates.map((plate, i) => {
+    //     return plate.div;
+    // }).join('');
+    for(let i =0;i<plates.length;i++) {
+        if(platesList.children[i]==null)
+        {
+            platesList.append(plates[i].div);
+        } else {
+            platesList.children[i].replaceWith(plates[i].div)
+        }
+    }
+}
+let moneyNumber = document.getElementById("moneyNumber");
+
 function breed() {
     console.log(selected);
-    console.log(`breed was called,selecteds:${selecteds},dad:${dad},mom:${mom},will breed:${(dad!=-1 && mom !=-1 && selecteds==2)}`);
-    if(dad!=-1 && mom !=-1 && selecteds==2)
+    const willBreed = (barshmallows[dad]!=null && barshmallows[mom]!=null);
+    console.log(`breed was called,selecteds:${selecteds},dad:${dad},mom:${mom},will breed:${(willBreed && selecteds==2)}`);
+    if(willBreed && selecteds==2)
     {
         breeding=true;
         breedDad=dad;
@@ -104,11 +151,11 @@ function breed() {
         breedLength=(barshmallows[dad].luck+barshmallows[mom].luck)*1000;
         
         buyCooldown+=15;
-        barshmallowId++;
         selected.push(false);
     }
 }
 function update() {
+    console.log("update started");
     moneyNumber.innerHTML=""+money;
     dad=-1;
     mom=-1;
@@ -135,12 +182,13 @@ function update() {
     });
     let bottom = document.getElementById("bottom");
     bottom.innerHTML = `You have ${numOfBarshmallows} barshmallows`;   
-    let breedtimer = document.getElementById("breedtimer");
-
+    populateList(barshmallows,barshmallowContainer)
+    console.log("update finished");
 }
 function animate() {
     requestAnimationFrame(animate);
     buyCooldown++;
+    let breedtimer = document.getElementById("breedtimer");
     if(breeding) {
         breedTime++;
         breedtimer.innerHTML=`Breeding... ${Math.round((breedTime/breedLength)*100)}%`;
@@ -176,7 +224,7 @@ function animate() {
                             -1// or nothing.
             )
         );
-        barshmallows[barshmallows.length-1].update();
+        // barshmallows[barshmallows.length-1].createDiv();
         breeding=false;
         breedTime=0;
         selected.forEach(s => {
@@ -201,25 +249,26 @@ function buy () {
         {
             barshmallows.push(new Barshmallow(Math.random()*20,1+Math.floor(Math.random()*6)*2,-1,Math.random(),true,-1));
             selected.push(false);
-            barshmallows[barshmallows.length-1].update();
+            // barshmallows[barshmallows.length-1].createDiv();
             barshmallowId++;
         } else {
             if(Math.random>0.5)
             {
                 barshmallows.push(new Barshmallow(Math.random()*20 ,1+Math.floor(Math.random()*6)*2 ,-1 ,Math.random() ,true ,-1));
                 selected.push(false);
-                barshmallows[barshmallows.length-1].update();
+                // barshmallows[barshmallows.length-1].createDiv();
                 barshmallowId++;
             } else {
                 console.log(hidden);
                 barshmallows[hidden].visible=true;
                 selected[hidden]=false;
-                barshmallows[hidden].update();
+                barshmallows[hidden].createDiv();
             }
         }
         money-=buyPrice;
         buyCooldown=0;
         buyPrice*=2;
+        update();
     }
 }
 function sell() {
@@ -249,38 +298,24 @@ function sell() {
         ));
         barshmallows[dad].visible=false;
         selected[dad]=false;
-        barshmallowContainer.removeChild(document.getElementById(""+dad));
+        // barshmallowContainer.removeChild(document.getElementById(""+dad));
+        update();
     }
 }
-//COOKIE FUNCTIONS
-function set_cookie ( cookie_name, cookie_value, lifespan_in_days, valid_domain )
-{
-  // https://www.thesitewizard.com/javascripts/cookies.shtml
-  var domain_string = valid_domain ? ("; domain=" + valid_domain) : '' ;
-  document.cookie = cookie_name + "=" + encodeURIComponent( cookie_value ) +
-      "; max-age=" + 60 * 60 * 24 * lifespan_in_days +
-      "; path=/" + domain_string ;
-}
-function get_cookie ( cookie_name )
-{
-  // https://www.thesitewizard.com/javascripts/cookies.shtml
-  var cookie_string = document.cookie ;
-  if (cookie_string.length != 0) {
-    var cookie_array = cookie_string.split( '; ' );
-    for (i = 0 ; i < cookie_array.length ; i++) {
-      cookie_value = cookie_array[i].match ( cookie_name + '=(.*)' );
-      if (cookie_value != null) {
-        return decodeURIComponent ( cookie_value[1] ) ;
-      }
-    }
-  }
-  return '' ;
-}
+//Save FUNCTIONS
 function save() {
-    //save barshmellows[], money
-    set_cookie("save",money,60);
+    //save barshmallows[], money
+    //set_cookie("save",money,60);
+    localStorage.setItem('barshmallows', JSON.stringify(barshmallows));
+    localStorage.setItem('money', JSON.stringify(money));
 }
 function load() {
-    //load save from cookie
-    console.log("save '"+get_cookie("save")+"'");
+    //load save
+    barshmallows = JSON.parse(localStorage.getItem('barshmallows'));
+    money = JSON.parse(localStorage.getItem('money'));
+}
+function newGame() {
+    money = 1000;
+    barshmallows = [new Barshmallow(Math.random()*20,3,-1,0.5000001,true,-1)];
+    update();
 }
